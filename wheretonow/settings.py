4c488a8b38b8c2d197f1ade438cf00e0b5a42998
sumitpathak92 <pathaksumit92@gmail.com>
2017-02-16 14:26:40 +0530

    settings changed

--- wheretonow/settings.py
+++ wheretonow/settings.py
@@ -115,7 +115,6 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
 )
 
 TEMPLATE_DIRS = (
